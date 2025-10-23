8d46470000495830ba6a5ab4a52baa204e0b161f
Marcos Souzza <marcossouza.git@gmail.com>
2025-10-23 08:45:50 -0300

    Criação da interface IBook dentro da libs

---
+++ libs/domain/book/book.ts
@@ -0,0 +1,52 @@
+// Configurando o Domínio do Book
+/**
+ * Interface que define a estrutura de dados (o Contrato)
+ * da nossa entidade Livro.
+ */
+export interface IBook {
+  id: string;
+  title: string;
+  author: string;
+  isbn: string;
+  condition: string;
+  price: number;
+  description: string;
+  stock: number; // Quantidade em estoque
+  coverImageUrl: string; //URL da imagem de capa
+  // Adicione outras propriedades (ex: anoPublicacao) conforme precisar.
+}
+
+/**
+ * Classe de Domínio do Livro.
+ * Ela implementa a interface e pode conter métodos de Regra de Negócio.
+ */
+export class Book implements IBook {
+  id: string;
+  title: string;
+  author: string;
+  isbn: string;
+  condition: string; //novo ou usado
+  price: number;
+  description: string;
+  stock: number;
+  coverImageUrl: string;
+
+  constructor(data: IBook) {
+    // Garantimos que todos os dados sejam atribuídos
+    Object.assign(this, data);
+  }
+
+  /**
+   * Método de Regra de Negócio: Verifica se o livro está disponível.
+   */
+  public isAvailable(): boolean {
+    return this.stock > 0;
+  }
+
+  /**
+   * Método de Apresentação/Utilidade: Formata o preço para exibição.
+   */
+  public getFormattedPrice(): string {
+    return `R$ ${this.price.toFixed(2).replace(".", ",")}`;
+  }
+}
